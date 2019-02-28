f4f51e9529b1aa1b19624b765ef136e430b1b632
vrillo <victor.rillo@keensoft.es>
2019-02-28 19:43:17 +0100

    Slide to string

--- src/main/java/es/keensoft/ghc2019/model/Slide.java
+++ src/main/java/es/keensoft/ghc2019/model/Slide.java
@@ -1,35 +1,38 @@
 package es.keensoft.ghc2019.model;
 
+import java.util.Iterator;
 import java.util.Set;
 
 public class Slide {
 
-	private Set<id> ids;
-	private Set<Photo> photos;
-	private Set<String> tags;
+    private Set<Photo> photos;
+    private Set<String> tags;
 
 
-	public Set<Photo> getPhotos() {
-		return photos;
-	}
+    public Set<Photo> getPhotos() {
+        return photos;
+    }
 
-	public void setPhotos(Set<Photo> photos) {
-		this.photos = photos;
-	}
+    public void setPhotos(Set<Photo> photos) {
+        this.photos = photos;
+    }
 
-	public Set<String> getTags() {
-		return tags;
-	}
+    public Set<String> getTags() {
+        return tags;
+    }
 
-	public void setTags(Set<String> tags) {
-		this.tags = tags;
-	}
+    public void setTags(Set<String> tags) {
+        this.tags = tags;
+    }
 
-	public Set<id> getIds() {
-		return ids;
-	}
 
-	public void setIds(Set<id> ids) {
-		this.ids = ids;
-	}
+    @Override
+    public String toString() {
+        String line = "";
+        for (Iterator<Photo> it = photos.iterator(); it.hasNext(); ) {
+            Photo photo = it.next();
+            line.concat(Integer.toString(photo.getId()));
+        }
+        return line;
+    }
 }
