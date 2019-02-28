01047b7d10fa4e19d4919319c7904b1e86b8016b
Daniel E. Fernández Pera <daniel.fernandez@keensoft.es>
2019-02-28 19:58:11 +0100

    FileReader completed

--- src/main/java/es/keensoft/ghc2019/model/Photo.java
+++ src/main/java/es/keensoft/ghc2019/model/Photo.java
@@ -4,11 +4,20 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Photo {
+	private long id;
 	private Orientation orientation;
 	private Set<String> tags = new HashSet<String>();
 
-	public Photo(String[] lineContent) {
-		// TODO Auto-generated constructor stub
+	public Photo(long id, String[] lineContent) {
+		this.id = id;
+		this.orientation = Orientation.valueOf(lineContent[0]);
+		int tagsNumber = Integer.parseInt(lineContent[1]);
+		for (int i = 2; i < lineContent.length; i++) {
+			tags.add(lineContent[i]);
+		}
+
+		if (tags.size() != tagsNumber)
+			throw new RuntimeException("No matching tags in photo " + id);
 	}
 
 	public Orientation getOrientation() {
@@ -27,9 +36,17 @@ public class Photo {
 		this.tags = tags;
 	}
 
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
 	@Override
 	public String toString() {
-		return "Photo [orientation=" + orientation + ", tags=" + tags + "]";
+		return "Photo [id=" + id + ", orientation=" + orientation + ", tags=" + tags + "]";
 	}
 
 }
---
+++ src/main/java/es/keensoft/ghc2019/reader/FileReader.java
@@ -0,0 +1,38 @@
+package es.keensoft.ghc2019.reader;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import es.keensoft.ghc2019.model.Photo;
+
+public class FileReader {
+	public static List<Photo> readFile(String path) throws IOException {
+		List<Photo> photos = new ArrayList<Photo>();
+		System.out.println("Reading file '" + path + "'...");
+
+		Scanner scanner = new Scanner(new File(path));
+		long lineNumber = 0;
+		long expectedNumber = 0;
+		while (scanner.hasNextLine()) {
+			String line = scanner.nextLine();
+			if (lineNumber == 0) {
+				expectedNumber = Long.parseLong(line);
+				System.out.println("Expected " + expectedNumber + " photos...");
+			} else {
+				String[] lineContent = line.split(" ");
+				Photo photo = new Photo(lineNumber -1, lineContent);
+				photos.add(photo);
+				System.out.println("\tAdded: " + photo);
+			}
+			lineNumber++;
+		}
+		scanner.close();
+
+		System.out.println(
+				"Expected items: '" + expectedNumber + "', lines parsed: '" + lineNumber + "', result items: '" + photos.size() + "'");
+		return photos;
+	}
+}
